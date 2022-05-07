556cefd8922231cdc78deaa6b9692365b2f1488e
muhammedraiyaan2 <muhammedraiyaan2@gmail.com>
2022-05-07 09:53:56 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Weather Forecast
+This website helps you to know about the weather example what's the temperature and weather type etc. This website is responsive that high level
+- - -
+# Contact us
+You can contact me in [GitHub](https://github.com/muhammedraiyaan2) or in my [Profile](https://muhammedraiyaan2.github.io/Profile/)
---
+++ logic.js
@@ -0,0 +1,19 @@
+let dataTxt=document.getElementById("data")
+let sub=document.getElementById("sub")
+let textData1=document.getElementById("textData1")
+let textData2=document.getElementById("textData2")
+let textData3=document.getElementById("textData3")
+sub.onclick=()=>{
+ let xml=new XMLHttpRequest();
+ xml.open("GET",`http://api.weatherapi.com/v1/current.json?key=9413ae19af5947609b4112935222404&q=${dataTxt.value}&aqi=no`,true)
+ xml.onload=(responseText)=>{
+  let dataJ =JSON.parse(xml.responseText)
+  let c = dataJ.current.temp_c
+  textData1.innerText=c
+  let f = dataJ.current.temp_f
+  textData2.innerText=f
+  let type = dataJ.current.condition["text"]
+  textData3.innerText=type
+ }
+ xml.send()
+}
