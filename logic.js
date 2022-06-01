0ca862b250571847767e4dbd07bb7c7af11ffdf8
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2022-06-01 18:14:00 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -5,7 +5,7 @@ let textData2=document.getElementById("textData2")
 let textData3=document.getElementById("textData3")
 sub.onclick=()=>{
  let xml=new XMLHttpRequest();
- xml.open("GET",`http://api.weatherapi.com/v1/current.json?key=9413ae19af5947609b4112935222404&q=${dataTxt.value}&aqi=no`,true)
+ xml.open("GET",`https://api.weatherapi.com/v1/current.json?key=9413ae19af5947609b4112935222404&q=${dataTxt.value}&aqi=no`,true)
  xml.onload=(responseText)=>{
   let dataJ =JSON.parse(xml.responseText)
   let c = dataJ.current.temp_c
